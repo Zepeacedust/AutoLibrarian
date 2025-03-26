943b4f922437e049ff2eaf26eee78e0f1fc3b4b3
Zepeacedust <arnor0903@gmail.com>
2025-03-26 19:59:28 +0000

    Diff distance spell finder

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}  -- allows "string literals" to be Text
 {-# LANGUAGE TemplateHaskell #-}
 module Main where
-
+import Data.List (minimumBy)
+import Data.Array ((!)) 
+import qualified Data.Array as Arr
+import           Data.Ord
 import           Data.List (find)
 import           Control.Monad (when, void)
 import           UnliftIO.Concurrent
@@ -42,28 +45,49 @@ data Spell = Spell {
 $(deriveJSON defaultOptions ''Spell)
 
 
+diff :: Text -> Text -> Int
+diff a b = memo 0 0 where
+    helperFunc :: Int -> Int -> Int
+    helperFunc x y
+        | T.index a x == T.index b y = modded
+        | otherwise = minimum  [
+            (1+subbed),
+            (1+added)
+            ]
+        where
+            modded = memo (x+1) (y+1)
+            added  = memo (x)   (y+1)
+            subbed = memo (x+1)   (y)
+    memo x y
+      | x >= lA = lB - y
+      | y >= lB = lA - x
+      | otherwise     = arr ! (x, y)
+    arr = Arr.listArray bound [helperFunc x y | (x,y) <- Arr.range bound]
+    bound = ((0,0),(lA, lB))
+    lA = T.length a
+    lB = T.length b
+
 readGrimoire fileName = do
     file <- B.readFile fileName
     let Just grimoire = decode file :: Maybe [Spell]
     return grimoire
 
 
-lookupSpell :: [Spell] -> Text -> Maybe Spell
-lookupSpell grimoire target = find (\s -> name s == toUpper target) grimoire
+lookupSpell :: [Spell] -> Text -> Spell
+lookupSpell grimoire target = minimumBy (comparing (diff (toUpper target) . name)) grimoire
 
-postSpell ::ChannelId -> Maybe Spell -> DiscordHandler ()
+postSpell ::ChannelId -> Spell -> DiscordHandler ()
 postSpell channel spell = do
     let spellCont = renderSpell spell
-    if T.length spellCont < 2000 
+    if T.length spellCont < 400 
         then
             void $ restCall (R.CreateMessage channel (renderSpell spell))
         else
             void $ restCall (R.CreateMessageDetailed channel (def {R.messageDetailedFile= Just ("spell.md", encodeUtf8 spellCont)}))
 
 
-renderSpell :: Maybe Spell -> Text
-renderSpell Nothing      = "Spell not found"
-renderSpell (Just spell) = "### " <> name spell<> "\n**" 
+renderSpell :: Spell -> Text
+renderSpell spell = "### " <> name spell<> "\n**" 
                             <> (fst.level$ spell) <> " " <> (snd.level $ spell) <> "**\n"
                             <> "**R:** " <> range spell <>", **D:** "<> duration spell <>", **T:** " <> target spell <> ", " <> T.intercalate ", " (tags spell) <> "\n" 
                             <> text spell <> "\n> "
